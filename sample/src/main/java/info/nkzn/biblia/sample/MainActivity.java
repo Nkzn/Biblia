869b198488dde42e122559367c8a4cdc0203818d
Yukiya Nakagawa <yn.airscope@gmail.com>
2016-08-11 18:54:13 +0900

    [sample] Add search box

--- sample/src/main/java/info/nkzn/biblia/sample/MainActivity.java
+++ sample/src/main/java/info/nkzn/biblia/sample/MainActivity.java
@@ -1,39 +1,56 @@
 package info.nkzn.biblia.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.List;
 
 import info.nkzn.biblia.Biblia;
 import info.nkzn.biblia.rakuten.Book;
 import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+    private EditText etKeyword;
+    private Button btnSearch;
+    private TextView tvResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Biblia.client().search("太陽")
+        etKeyword = (EditText) findViewById(R.id.et_keyword);
+        btnSearch = (Button) findViewById(R.id.btn_search);
+        tvResult = (TextView) findViewById(R.id.tv_result);
+
+        btnSearch.setOnClickListener(v -> search());
+    }
+
+    private void search() {
+        String keyword = etKeyword.getText().toString();
+        if (TextUtils.isEmpty(keyword)) {
+            return;
+        }
+
+        Biblia.client().search(keyword)
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Action1<List<Book>>() {
-                    @Override
-                    public void call(List<Book> books) {
-                        Log.d(TAG, books.toString());
-                    }
-                }, new Action1<Throwable>() {
-                    @Override
-                    public void call(Throwable throwable) {
-                        Log.w(TAG, throwable);
-                    }
-                });
+                .subscribe(this::onSearchFinished, this::onError);
+    }
+
+    private void onSearchFinished(List<Book> books) {
+        tvResult.setText(books.toString());
+    }
+
+    private void onError(Throwable throwable) {
+        tvResult.setText(String.format("Error:\n%s", throwable.getMessage()));
     }
 }
